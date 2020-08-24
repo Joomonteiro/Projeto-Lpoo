fb4347f1dce777bdf4f1cd0bcab22a05327ee5f7
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2020-08-24 17:24:40 -0300

    Adicionados os atributos e métodos abstratos para classe abstrataConta

--- Conta.java
+++ Conta.java
@@ -1,5 +1,54 @@
 package ProjetoLpooSistemaDeBanco;
 
-public class Conta {
-
+public abstract class Conta {
+	public String numero;
+	public String agencia;
+	public String nomeagencia;
+	public String nome;
+	public double saldo;
+	public String cpf;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+	public String getNomeagencia() {
+		return nomeagencia;
+	}
+	public void setNomeagencia(String nomeagencia) {
+		this.nomeagencia = nomeagencia;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public abstract void realizadeposito(double deposito);
+	
+	public abstract void realizasaque(double saque);
+	
+	public abstract void renderjuros();
+	
 }
